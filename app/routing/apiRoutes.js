1d0ad7a3bc6d26333a1bd0f25ea082c4c94c1b5c
Vahe Minasyan (Red Door Collaborative LLC) <v-vamin@microsoft.com>
2019-01-19 03:25:22 -0800

    built feature to grab user's data and add it to the data file

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,8 +5,17 @@ const path = require("path");
 var friendData = require("../data/friends");
 
 // Sets up routing 
-module.exports = function(app) {
+module.exports = function (app) {
+
+    // GET request to display raw data in json format
     app.get("/api/friends", function (req, res) {
         res.json(friendData)
     });
+
+    // POST request to send incoming survey results into the friendArray
+    app.post("/api/friends", function (req, res) {
+        friendData.push(req.body);
+    })
+
 };
+
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -10,10 +10,6 @@ module.exports = function (app) {
     app.get("/survey", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"))
     });
-    
-    // app.get("*", function (req, res) {
-    //     res.sendFile(path.join(__dirname, "../public/home.html"))
-    // });
 };
 
 
